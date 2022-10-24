47fdc219a4bf1fc6be98cbf23ff26aca897a5c3d
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2022-10-25 02:30:39 +0400

    movement

---
+++ Assets/Armaan/Scripts/movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private CharacterController controller;
+    public static float speed = 5.0f;
+    public static bool move = true;
+    public static bool jump = true;
+    private float yvelocity;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        velocity.y = yvelocity;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
