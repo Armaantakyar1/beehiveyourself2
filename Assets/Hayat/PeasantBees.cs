88bb057e0ee95038b4bf25c0016e6e0ba8dc86c7
HatAYE <h.alchaban@student.saedubai.com>
2022-11-01 10:38:52 +0400

    targets list made but doesnt work yet

--- Assets/Hayat/PeasantBees.cs
+++ Assets/Hayat/PeasantBees.cs
@@ -4,32 +4,76 @@ using UnityEngine;
 
 public class PeasantBees : MonoBehaviour
 {
-    [SerializeField] Vector3 Target;
-    [SerializeField] GameObject LeaderBee;
+   // [SerializeField] Vector3 Target;
+    //[SerializeField] GameObject LeaderBee;
+    Targets target;
     Vector3 Direction;
-    float MaximumVelocity=10f;
-    int speed = 2;
+    [SerializeField] float MaximumVelocity=10f;
+    [SerializeField] float SlowingDownDistance;
+    [SerializeField] float StopDistance;
+    [SerializeField] float distance;
+    [SerializeField] float speed = 2;
     [SerializeField]Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
 
     void Update()
     {
-                SeekLeaderBee();
+        SeekLeaderBee();
+        Truncate();
+        //Arrive();
     }
 
     void SeekLeaderBee()
     {
-        Target = LeaderBee.transform.position;
-        Direction = (Target - transform.position).normalized * speed;
+        target.Target1 = target.LeaderBee.transform.position;
+        Direction = (target.Target1 - transform.position).normalized * speed;
         rb.velocity += Direction;
         if (rb.velocity.magnitude> MaximumVelocity)
         {
             rb.velocity = rb.velocity.normalized * MaximumVelocity;
+        }
+    }
+
+    float Arrive()
+    {
+        distance = Vector3.Distance(a: target.Target1, b: transform.position);
+        /*if (distance>SlowingDownDistance)
+        {
+            return 1;
+        }
+
+        if (distance<SlowingDownDistance)
+        {
+            return 0;
+        }*/
+        if (SlowingDownDistance==5)
+        {
+            return 5;
+        }
+        if (SlowingDownDistance == 3)
+        {
+            return 3;
+        }
+        if (SlowingDownDistance == 2)
+        {
+            return 2;
+        }
+        speed = 1 / (SlowingDownDistance - StopDistance);
+        return speed * (distance - StopDistance);
+
+
+    }
+    void Truncate()
+    {
+        speed = Arrive();
+        if (rb.velocity.magnitude > speed)
+        {
+            rb.velocity = rb.velocity.normalized * speed ;
         }
     }
 }
