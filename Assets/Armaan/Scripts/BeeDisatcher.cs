6e02706e4cf0d89ca9cb178799f93ceb3f047944
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2022-12-04 17:14:32 +0400

    minor

--- Assets/Armaan/Scripts/BeeDisatcher.cs
+++ Assets/Armaan/Scripts/BeeDisatcher.cs
@@ -24,11 +24,4 @@ using UnityEngine;
 
         }
 
-        protected void AddBeToFlower(FlowerBehaviour selected)
-        {
-            /// 
-            /// /// if yes give it one of the bees and remove it from swarm the list
-            /// // tell the bee you removed to follow the flower instead of follwing the swarm
-            /// 
-        }
     }
