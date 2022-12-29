7ba5f00e181538a40421e6b5ad0facca9f8188b7
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2022-12-29 20:21:55 +0400

    fixed build issues

--- Assets/Hayat/Scripts/Level_Loader/EditorSpawn.cs
+++ Assets/Hayat/Scripts/Level_Loader/EditorSpawn.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-public class EditorSpawn : EditorWindow
-{
-    string name = "";
-    GameObject prefabToSpawn;
-    int radius;
+//public class EditorSpawn : EditorWindow
+//{
+//    string name = "";
+//    GameObject prefabToSpawn;
+//    int radius;
 
-    [MenuItem("Hayat's stuff/Hayat's Tools")]
-    public static void ShowWindow()
-    {
-        GetWindow(typeof(EditorSpawn));
-    }
-    private void OnGUI()
-    {
-        name = EditorGUILayout.TextField("Object name", name);
-        radius = EditorGUILayout.IntField("Radius", radius);
-        prefabToSpawn = EditorGUILayout.ObjectField("Prefab To Spawn", prefabToSpawn, typeof(GameObject), false) as GameObject;
-        if(GUILayout.Button("Spawn Object"))
-        {
-            Spawn();
-        }
-    }
+//    //[MenuItem("Hayat's stuff/Hayat's Tools")]
+//    public static void ShowWindow()
+//    {
+//        GetWindow(typeof(EditorSpawn));
+//    }
+//    private void OnGUI()
+//    {
+//        name = EditorGUILayout.TextField("Object name", name);
+//        radius = EditorGUILayout.IntField("Radius", radius);
+//        prefabToSpawn = EditorGUILayout.ObjectField("Prefab To Spawn", prefabToSpawn, typeof(GameObject), false) as GameObject;
+//        if (GUILayout.Button("Spawn Object"))
+//        {
+//            Spawn();
+//        }
+//    }
 
-    void Spawn()
-    {
-        Vector3 pos = Random.insideUnitCircle * 5;
-        GameObject obj =  Instantiate(prefabToSpawn, pos, Quaternion.identity);
-        obj.name = name;
-    }
+//    void Spawn()
+//    {
+//        Vector3 pos = Random.insideUnitCircle * 5;
+//        GameObject obj = Instantiate(prefabToSpawn, pos, Quaternion.identity);
+//        obj.name = name;
+//    }
 
-}
+//}
